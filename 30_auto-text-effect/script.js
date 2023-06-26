7fb2d4d575b421f174adf825c060e511d668028a
OscarHF NTT <raxdev.frontier@gmail.com>
2023-06-26 16:31:05 +0200

    auto-text-effect-01

---
+++ 30_auto-text-effect/script.js
@@ -0,0 +1,29 @@
+const textEl = document.getElementById("text")
+const speedEl = document.getElementById("speed")
+const newTextEl = document.getElementById("newText")
+let text = "Starting...   ...end"
+
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+	textEl.innerText = text.slice(0, idx)
+
+	idx++
+
+	if (idx > text.length) {
+		idx = 1
+	}
+
+	setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener("input", (e) => {
+	speed = 300 / e.target.value
+})
+
+newTextEl.addEventListener("change", (e) => {
+	text = e.target.value
+})
