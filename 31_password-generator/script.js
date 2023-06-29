358b780f13f8cb4074b9ec99dc25faafd06657d9
OscarHF NTT <raxdev.frontier@gmail.com>
2023-06-29 14:22:23 +0200

    password-generator-02

--- 31_password-generator/script.js
+++ 31_password-generator/script.js
@@ -1,3 +1,22 @@
+/* CHALLENGE
+	Con el codigo actual, la contraseña siempre mantendrá el patron establecido en "randomFunc"
+	es decir, la contraseña generada siempre tendra el formato: 
+	(lowerCase)(uppercase)(number)(symbol) - por ejemplo: aB6% o hP1/
+
+	en funcion claro de si esta marcada la checkbox correspondiente
+
+	como CHALLENGE se puede hacer que este patron sea aleatorio cada vez 
+*/
+
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const uppercaseEl = document.getElementById("uppercase")
+const lowercaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
 const randomFunc = {
 	lower: getRandomLower,
 	upper: getRandomUpper,
@@ -5,6 +24,64 @@ const randomFunc = {
 	symbol: getRandomSymbol,
 }
 
+clipboardEl.addEventListener("click", () => {
+	// const textarea = document.createElement("textarea")
+	// const password = resultEl.innerText
+
+	// if (!password) {
+	// 	return
+	// }
+
+	// textarea.value = password
+	// document.body.appendChild(textarea)
+	// textarea.select()
+	// document.execCommand("copy")
+	// textarea.remove()
+	// alert("Password copied to clipboard!")
+
+	const password = resultEl.innerText
+
+	if (!password) {
+		return
+	}
+	navigator.clipboard
+		.writeText(password)
+		.then(alert("Password copied to clipboard!"))
+		.catch(alert("Error copying password"))
+})
+
+generateEl.addEventListener("click", () => {
+	//amb el "+" davant 'parsegem' el valor de lengthEl.value (es a dir el valor del input de la logitud de la password)
+	const howLength = +lengthEl.value
+	const hasLower = lowercaseEl.checked
+	const hasUpper = uppercaseEl.checked
+	const hasNumber = numbersEl.checked
+	const hasSymbol = symbolsEl.checked
+
+	resultEl.innerText = generatePass(hasLower, hasUpper, hasNumber, hasSymbol, howLength)
+})
+
+function generatePass(lower, upper, number, symbol, length) {
+	let generatedPass = ""
+	const typesCount = lower + upper + number + symbol
+	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+		(item) => Object.values(item)[0]
+	)
+
+	if (typesCount === 0) {
+		return ""
+	}
+
+	for (let i = 0; i < length; i += typesCount) {
+		typesArr.forEach((type) => {
+			const functName = Object.keys(type)[0]
+			generatedPass += randomFunc[functName]()
+		})
+	}
+
+	const finalPass = generatedPass.slice(0, length)
+	return finalPass
+}
 function getRandomLower() {
 	/* Math.random() * (max - min) + min; 
     el codigo anterior devuelve un número aleatorio situado entre dos valores específicos. 
