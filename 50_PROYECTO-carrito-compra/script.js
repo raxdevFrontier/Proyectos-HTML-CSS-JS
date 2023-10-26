58132be47b12b1d7a86929dd91d2c4f884fa3802
OscarHF NTT <raxdev.frontier@gmail.com>
2023-10-26 15:43:59 +0200

    CARRITO-02_leer datos curso que se quiere agregar

--- 50_PROYECTO-carrito-compra/script.js
+++ 50_PROYECTO-carrito-compra/script.js
@@ -6,17 +6,34 @@ const vaciarCarrito = document.querySelector('#vaciar-carrito')
 cargarEventListeners()
 function cargarEventListeners() {
     //Cuando se agrega un curso, presionando el botón "Agregar al carrito" 
-    listaCursos.addEventListener('click', agregarCurso)
+    listaCursos.addEventListener('click', agregarCursoBtn)
 }
 
 
-//Funciones
-function agregarCurso(e) {
+//FUNCIONES
+//Accediendo al botón "Agregar al carrito"
+function agregarCursoBtn(e) {
     //aplicamos el preventDefault porque "Agregar al carrito" es en realidad un link (con # como enlace)
     // por lo que redirige al inicio de la pagina como comportamiento predeterminado, y hay que eliminar ese comportamiento 
     e.preventDefault()
 
     if(e.target.classList.contains('agregar-carrito')){
-        console.log('Agregaste al carrito')
+        const cursoSeleccionado = e.target.parentElement.parentElement
+
+        leerDatosCurso(cursoSeleccionado)
     }
+}
+
+//Funcion para leer los datos del curso que se quiere agregar al carrito
+function leerDatosCurso(curso) {
+    
+    //Creamos objeto con datos del curso seleccionado
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso)
 }
