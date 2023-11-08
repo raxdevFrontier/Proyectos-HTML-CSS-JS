6f127acfe5fc6afd240fc1b73d1763c57e8beb8a
OscarHF NTT <raxdev.frontier@gmail.com>
2023-11-08 11:52:57 +0100

    TWEETS-02_agregar y mostrar tweets

--- 53_PROYECTO-almacenar-en-localStorage/app.js
+++ 53_PROYECTO-almacenar-en-localStorage/app.js
@@ -27,6 +27,27 @@ function agregarTweet(e){
         mostrarError('No se puede agregar tweets vacios')
         return
     }
+
+    //añadir a lista de Tweets
+    const tweetObj = {
+        id: Date.now(),
+        tweet: tweet 
+        // tweet
+        /*recientement en JS se ha añadido que si al crear un obj 
+        se usa la misma palabra para la clave y el valor de una propiedad
+        la sitaxis puede pasar de ser -> "palabra: palabra"
+        a ser -> "palabra" ( a secas )
+        */
+    }
+
+    //añadir tweet al arreglo de tweets
+    tweets = [...tweets, tweetObj]
+
+    //agregar HTML de la lista de Tweets
+    crearHTML()
+
+    //reiniciar formulari
+    formulario.reset()
 }
 
 function mostrarError(error){
@@ -40,4 +61,26 @@ function mostrarError(error){
     setTimeout(() => {
         mensajeError.remove()
     }, 3000)
+}
+
+//añadir HTML de los tweets que se quieren añadir
+function crearHTML(){
+
+    limpiarHTML()
+
+    if(tweets.length > 0){
+        tweets.forEach(tweet => {
+            //crear el HTML
+            const li = document.createElement('li')
+            li.innerText = tweet.tweet
+            listaTweets.appendChild(li)
+        })
+    }
+}
+
+//limpiar HTML lista tweets
+function limpiarHTML(){
+    while(listaTweets.firstChild){
+        listaTweets.removeChild(listaTweets.firstChild)
+    }
 }
