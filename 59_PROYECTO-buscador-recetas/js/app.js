b29db0a627e6924980efb327009738ac55d172ce
OscarHF NTT <raxdev.frontier@gmail.com>
2023-12-01 10:36:33 +0100

    RECETAS-02_mostrar categorias en el Select

--- 59_PROYECTO-buscador-recetas/js/app.js
+++ 59_PROYECTO-buscador-recetas/js/app.js
@@ -1,11 +1,24 @@
 function iniciarApp() {
+	const selectCategorias = document.querySelector('#categorias')
+
 	obtenerCategorias()
 
 	function obtenerCategorias() {
 		const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
 		fetch(url)
 			.then((respuesta) => respuesta.json())
-			.then((resultado) => console.log(resultado))
+			.then((resultado) => mostrarCategorias(resultado.categories))
+	}
+
+	function mostrarCategorias(categorias) {
+		categorias.forEach((categoria) => {
+			const { strCategory } = categoria
+			const option = document.createElement('option')
+			option.value = strCategory
+			option.textContent = strCategory
+			selectCategorias.appendChild(option)
+			// console.log(option)
+		})
 	}
 }
 
