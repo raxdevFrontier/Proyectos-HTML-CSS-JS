7481df5f12e5c01db729fc7f9029fd3bfcedf470
OscarHF NTT <raxdev.frontier@gmail.com>
2023-12-12 11:37:02 +0100

    CRIPTOS-02_leer moneda y cripto + validar formulario

--- 61_PROYECTO-cotizado-crypto/js/app.js
+++ 61_PROYECTO-cotizado-crypto/js/app.js
@@ -1,4 +1,11 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas')
+const monedaSelect = document.querySelector('#moneda')
+const formulario = document.querySelector('#formulario')
+
+const objBusqueda = {
+	moneda: '',
+	criptomoneda: '',
+}
 
 //crear Promise
 const obtenerCriptomonedas = (criptomonedas) =>
@@ -8,6 +15,11 @@ const obtenerCriptomonedas = (criptomonedas) =>
 
 document.addEventListener('DOMContentLoaded', () => {
 	consultarCriptos()
+
+	formulario.addEventListener('submit', submitFormulario)
+
+	criptomonedasSelect.addEventListener('change', leerValor)
+	monedaSelect.addEventListener('change', leerValor)
 })
 
 function consultarCriptos() {
@@ -29,4 +41,48 @@ function selectCriptomonedas(criptomonedas) {
 		option.textContent = FullName
 		criptomonedasSelect.appendChild(option)
 	})
+}
+
+function leerValor(e) {
+	objBusqueda[e.target.name] = e.target.value
+	console.log(objBusqueda)
+}
+
+function submitFormulario(e) {
+	e.preventDefault()
+
+	const { moneda, criptomoneda } = objBusqueda
+
+	if (moneda === '' || criptomoneda === '') {
+		mostrarAlerta('Ambos campos obligatorios')
+		return
+	}
+}
+
+function mostrarAlerta(mensaje) {
+	const existeAlerta = document.querySelector('.bg-red-200')
+
+	if (!existeAlerta) {
+		const alerta = document.createElement('p')
+		alerta.classList.add(
+			'bg-red-200',
+			'border-red-600',
+			'text-red-700',
+			'px-4',
+			'py-3',
+			'rounded',
+			'mx-auto',
+			'mt-6',
+			'text-center',
+			'text-4xl'
+		)
+
+		alerta.textContent = mensaje
+
+		formulario.appendChild(alerta)
+
+		setTimeout(() => {
+			alerta.remove()
+		}, 3000)
+	}
 }
