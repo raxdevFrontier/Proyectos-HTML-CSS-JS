c9784b4611be3b27200f8403ca3b6028936c4a53
OscarHF NTT <raxdev.frontier@gmail.com>
2023-12-13 17:13:08 +0100

    CRUD_05-enviar peticion POST al servidor con nuevo cliente

---
+++ 64_PROYECTO-CRM-Crud-REST/js/API.js
@@ -0,0 +1,18 @@
+const url = 'http://localhost:4000/clientes'
+
+export const nuevoCliente = async (cliente) => {
+	try {
+		await fetch(url, {
+			//datos de confuracion
+			method: 'POST', //metodo que se realiza, en este caso enviar datos a la API
+			body: JSON.stringify(cliente), //contenido de la peticion (string o object)
+			headers: {
+				//informacion del tipo de datos que mandamos
+				'Content-Type': 'application/json', //en este caso el "content-type" es de tipo JSON
+			},
+		})
+		window.location.href = 'index.html'
+	} catch (error) {
+		console.log(error)
+	}
+}
