a362d907e5c14b796b53fd911cc2aa645f9430f0
OscarHF NTT <raxdev.frontier@gmail.com>
2023-12-13 18:09:52 +0100

    CRUD-08_obtener datos cliente para editar

--- 64_PROYECTO-CRM-Crud-REST/js/API.js
+++ 64_PROYECTO-CRM-Crud-REST/js/API.js
@@ -38,3 +38,14 @@ export const eliminarCliente = async (id) => {
 		console.log(error)
 	}
 }
+
+// obtener un cliente para edicion por su ID
+export const obtenerCliente = async (id) => {
+	try {
+		const resultado = await fetch(`${url}/${id}`)
+		const cliente = await resultado.json()
+		return cliente
+	} catch (error) {
+		console.log(error)
+	}
+}
