de26f2f556d03153fba067143accd94e6d248f78
OscarHF NTT <raxdev.frontier@gmail.com>
2023-12-22 13:36:58 +0100

    REACT-04_intro components + crear componente Header

--- 65_REACT-cotizador-prestamos/src/App.jsx
+++ 65_REACT-cotizador-prestamos/src/App.jsx
@@ -1,11 +1,12 @@
+import Header from './components/Header'
+
 function App() {
 
   /* Codigo JavaScript*/
-  const hola = 'hola mundo'
   return (
     /* Codigo HTML */
     <div className="my-2 max-w-lg mx-auto bg-white shadow p-10">
-      {hola}
+      <Header/>
     </div>
   )
 }
---
+++ 65_REACT-cotizador-prestamos/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+
+    return(
+        <h1 className="text-4xl font-extrabold text-center text-gray-500">
+            ¿Cuanto <span className="text-indigo-600">dinero </span> necesitas?
+        </h1>
+    )
+}
+
+export default Header
