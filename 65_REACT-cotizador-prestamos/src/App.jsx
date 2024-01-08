aab91c5964745e0e64128ed539fb596776df3655
OscarHF NTT <raxdev.frontier@gmail.com>
2024-01-08 08:57:59 +0100

    REACT-05_añadiendo un input

--- 65_REACT-cotizador-prestamos/src/App.jsx
+++ 65_REACT-cotizador-prestamos/src/App.jsx
@@ -7,6 +7,11 @@ function App() {
     /* Codigo HTML */
     <div className="my-2 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
+
+      <input 
+        type="range" 
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600" 
+        />
     </div>
   )
 }
